9a4914b3e39a45356266ccfe515bc676c16e7604
YSB_uncyclocity <seongbeom_lee@kakao.com>
2021-08-27 15:04:22 +0900

    [C. U.] 홈페이지 스크린샷 받을 수 있도록 설정
    w/ Node.js + PhantomJS

--- mailer/routes/index.js
+++ mailer/routes/index.js
@@ -1,12 +1,33 @@
 var express = require("express");
 var router = express.Router();
 const mailer = require("./mail");
+const phantom = require("phantom");
 
 /* GET home page. */
-router.get("/mail", function (req, res, next) {
+router.get("/mail", async function (req, res, next) {
   const { email } = req.body;
 
-  console.log(req.body);
+  await phantom
+    .create()
+    .then((ph) => {
+      _ph = ph;
+      return _ph.createPage();
+    })
+    .then()
+    .then((page) => {
+      _page = page;
+      _page.property("viewportSize", { width: 1920, height: 1000 });
+      return _page.open("http://www.shift.co.kr/");
+    })
+    .then((status) => {
+      return _page.render("test.png");
+    })
+    .then((status) => {
+      return _page.close();
+    })
+    .then((status) => {
+      return _ph.exit();
+    });
 
   let emailParam = {
     toEmail: email,
